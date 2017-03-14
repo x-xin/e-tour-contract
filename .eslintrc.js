77996f691deabe75c34f28be5cfc14c6582b5d3b
Roda <2015@roda.wang>
2017-03-14 21:07:50 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,8 @@ module.exports = {
   ],
   // add your custom rules here
   'rules': {
+    // allow no-unused-vars
+    "no-unused-vars": 0,
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
